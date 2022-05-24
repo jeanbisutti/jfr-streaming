0df60d7bc1bc984ba2e6ee8f58b6c6d5fd26b5bf
jeanbisutti <jean.bisutti@gmail.com>
2022-05-24 16:44:20 +0200

    Ability to configure JFR events with a map

---
+++ src/test/java/com/microsoft/jfr/ProgrammaticJfrEventConfigTest.java
@@ -0,0 +1,81 @@
+package com.microsoft.jfr;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+public class ProgrammaticJfrEventConfigTest {
+    private FlightRecorderConnection flightRecorderConnection = null;
+
+    @BeforeTest
+    public void setup() {
+        flightRecorderConnection = RecordingTest.getFlightRecorderConnection();
+    }
+
+    @AfterTest
+    public static void tearDown() {
+        RecordingTest.tearDown();
+    }
+
+    @Test
+    public void programmaticEventConfigWithMap() {
+
+        Map<String, String> recordingConfigAsMap = new HashMap<>();
+        recordingConfigAsMap.put("jdk.ObjectAllocationInNewTLAB#enabled", "true");
+        recordingConfigAsMap.put("jdk.ObjectAllocationOutsideTLAB#enabled", "true");
+
+        RecordingConfiguration recordingConfiguration = new RecordingConfiguration.MapConfiguration(recordingConfigAsMap);
+
+        executeRecording(recordingConfiguration);
+
+    }
+    private void executeRecording(RecordingConfiguration recordingConfiguration) {
+        Path dumpFile = null;
+        try {
+            dumpFile = Paths.get("target", "testRecordingDump_dumped.jfr");
+            Files.deleteIfExists(dumpFile);
+
+            RecordingOptions noRecordingOption = null;
+            Recording recording = flightRecorderConnection.newRecording(noRecordingOption, recordingConfiguration);
+            long id = recording.start();
+            Instant now = Instant.now();
+            Instant then = now.plusSeconds(1);
+            while (Instant.now().compareTo(then) < 0) {
+                RecordingTest.fib(Short.MAX_VALUE); // do something
+            }
+            recording.stop();
+            recording.dump(dumpFile.toString());
+            assertTrue(Files.exists(dumpFile));
+        } catch (IllegalArgumentException badData) {
+            fail("Issue in test data: " + badData.getMessage());
+        } catch (IOException ioe) {
+            // possible that this can be thrown, but should not happen in this context
+            fail("IOException not expected: ", ioe);
+        } catch (JfrStreamingException badBean) {
+            fail("Error thrown by MBean server or FlightRecorderMXBean: ", badBean);
+        } finally {
+            if (dumpFile != null) {
+
+                /*
+                try {
+                    Files.deleteIfExists(dumpFile);
+                } catch (IOException ignore) {
+                }
+                 */
+            }
+        }
+    }
+
+}
